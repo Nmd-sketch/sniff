200c31724950cc12640f70fb33b732c35e00c64d
Nmd-sketch <revusarlic@gmail.com>
2026-08-20 18:06:11 -0400

    Read UTF-8 command line via CommandLineToArgvW

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,58 @@
 #include <memory>
 #include <string>
 #include <vector>
-
+#ifdef _WIN32 
+    #include <windows.h>
+#endif
 #include "application/service/SearchService.hpp"
 #include "infrastructure/scanner/Win32Scanner.hpp"
 #include "interface/cli/CliController.hpp"
 
-int main(int argc, char** argv) {
-    std::vector<std::string> args(argv + 1, argv + argc);
+static std::string WideToUTF8(const std::wstring& wstr) {
+    if (wstr.empty()) return std::string();
+
+    int size = WideCharToMultiByte(
+        CP_UTF8,
+        0,
+        wstr.c_str(),
+        (int)wstr.length(),
+        nullptr,
+        0,
+        nullptr,
+        nullptr
+    );
+
+    if (size <= 0) return std::string();
+
+    std::string res(size, 0);
+    WideCharToMultiByte(
+        CP_UTF8,
+        0,
+        wstr.c_str(),
+        (int)wstr.length(),
+        &res[0],
+        size,
+        nullptr,
+        nullptr
+    );
+
+    return res;
+}
+
+static std::vector<std::string> parse_cmd() {
+    int arg_num = 0;
+    std::vector<std::string> results;
+LPWSTR* l = CommandLineToArgvW(GetCommandLineW(), &arg_num);
+    for (int i = 1; i < arg_num; ++i) {
+        results.push_back(WideToUTF8(l[i]));
+    }
+    LocalFree(l);
+    return results;
+}
+
+
+int main() {
+    std::vector<std::string> args = parse_cmd();
 
     infrastructure::Win32Scanner scanner;
     application::SearchService service(scanner);
